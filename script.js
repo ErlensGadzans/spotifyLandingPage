7a9a1f4b7dd503999f2413055e30e38659be4334
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-04-08 12:54:21 +0300

    JS is creating divs & classNames & displaying cards with requested info

--- script.js
+++ script.js
@@ -9,32 +9,54 @@ async function loadSongs(artist) {
       },
     }
   );
-
   let songsResponse = await response.json();
   console.log(songsResponse.data);
   return songsResponse.data;
 }
 
-window.onload = async function () {
-  let bonJoviSongs = await loadSongs("bon_jovi");
+async function renderSongs(artist) {
+  let songs = await loadSongs(artist);
   let songsDiv = document.getElementById("songs");
 
-  bonJoviSongs.forEach(
+  // Creating divs with class names by JS
+  // <div> Title </div>
+  //<div>song inside </div>
+
+  let artistSection = document.createElement("div"); //creating parent div
+  let title = document.createElement("h2"); //creating title of the section
+  title.innerText = "Songs from " + artist; // setting text for it
+  artistSection.appendChild(title); //adding title to the section
+
+  let songsContainer = document.createElement("div"); //creating container for my songs
+  songsContainer.classList.add("song-list"); //adding classList for the future needs
+
+  songs.forEach(
     (song) =>
-      (songsDiv.innerHTML += `<div class="song-card">
-    <img
-      class="song-card-image"
-      alt="here supposed to be a picture"
-      src ="${song.album.cover_medium}"
-    />
-    <div class="song-card-info">
-      <div class="song-card-artist">${song.artist.name}</div>
-      <div class="song-card-album">${song.album.title}</div>
-      <div class="song-card-title">
-        ${song.title}
-      </div>
-    </div>
-    <div class="song-card-play"></div>
-  </div>`)
+      (songsContainer.innerHTML +=
+        //forEach sing I use card, which was made previously
+        `<div class="song-card">
+<img
+  class="song-card-image"
+  alt="here supposed to be a picture"
+  src ="${song.album.cover_medium}"
+/>
+<div class="song-card-info">
+  <div class="song-card-artist">${song.artist.name}</div>
+  <div class="song-card-album">${song.album.title}</div>
+  <div class="song-card-title">
+    ${song.title}
+  </div>
+</div>
+<div class="song-card-play"></div>
+</div>`)
   );
+
+  artistSection.appendChild(songsContainer); //adding the songs container to the artist section
+
+  songsDiv.appendChild(artistSection); //adding the artist section to the page
+}
+
+window.onload = async function () {
+  renderSongs("metallica");
+  renderSongs("bon_jovi");
 };
